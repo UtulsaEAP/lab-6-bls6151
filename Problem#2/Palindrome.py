518a4691a174e4d1f55d7e5bf8e37e7b23cf7302
Braden Stanfield <bls6151@utulsa.edu>
2024-03-01 14:12:53 -0600

    Problem 2

--- Problem#2/Palindrome.py
+++ Problem#2/Palindrome.py
@@ -1,6 +1,17 @@
 #Name: Braden Stanfield  Lab: Fri 3
 def check_palindrome(user_input):
  #type your code here
+    back = ""
+    leng = len(user_input)
+    i = leng - 1
+    while i > -1:
+        back += user_input[i]
+        i -= 1
+    if user_input == back:
+        print("palindrome: " + user_input)
+    else:
+        print("not a palindrome: " + user_input)
+    
 if __name__ == "__main__":
     user_input = input()
     check_palindrome(user_input)
