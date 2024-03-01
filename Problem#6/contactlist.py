f07b9838fc825ee492b95280016252ba6d2ff379
Braden Stanfield <bls6151@utulsa.edu>
2024-03-01 14:53:18 -0600

    Still working on 6

--- Problem#6/contactlist.py
+++ Problem#6/contactlist.py
@@ -1,16 +1,18 @@
 #Name: Braden Stanfield  Lab: Fri 3
 def process_user_contacts(user_input):
-    user_contacts = 
+    i = 0
+    
+    user_contacts = 0
 
  
-    user_input = 
-    tokens = 
+    user_input = 0
+    tokens = 0
 
     # Put word pairs into a dictionary
     
     # Get contact name from input, output contact's phone number
     contact_name = input("Enter the contact name: ")
-    
+    print(0)
    
 if __name__ == '__main__':
     # Get input for word pairs
