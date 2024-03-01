388b27532114db80c9a117d2764cec90e0bdfeb3
Braden Stanfield <bls6151@utulsa.edu>
2024-03-01 14:34:28 -0600

    Problem 4

--- Problem#4/filter.py
+++ Problem#4/filter.py
@@ -1,14 +1,25 @@
 #Name: Braden Stanfield  Lab: Fri 3
 def process_and_print(input_string):
       # Split into separate strings
-
+    nums = input_string.split()
+    i = 0
     # Convert strings to integers and filter out negative values
-    input_data = 
+    input_data = []
+    while i < len(nums):
+        if int(nums[i]) < 0:
+            input_data.append(int(nums[i])) 
+        i += 1
 
     # Sort integers in reverse order
-  
+    input_data.sort()
+    input_data.reverse()
     # Print sorted integers
-    
+    j = 0
+    printed = ""
+    while j < len(input_data):
+        printed += (str(input_data[j]) + " ")
+        j += 1
+    print(printed)
 
 if __name__ == "__main__":
     # User inputs string w/ numbers
