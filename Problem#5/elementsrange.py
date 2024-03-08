311a8482ac52e14ec6567964e32d86193c62575d
Braden Stanfield <bls6151@utulsa.edu>
2024-03-08 15:24:25 -0600

    4 and 5 done

--- Problem#5/elementsrange.py
+++ Problem#5/elementsrange.py
@@ -8,7 +8,7 @@ def filter_and_print_range(input_list, min_val, max_val):
             if int(input_list[i]) <= max_val:
                 nums += str(input_list[i]) + ","
         i += 1
-    print(nums)
+    print(nums, end="")
 if __name__ == '__main__':
     # Get input for the list of integers
     user_input = input("Enter a space-separated string of numbers: ")
