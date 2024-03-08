a6862203267cdd4b7775e3d72dba33427da0c6cc
Braden Stanfield <bls6151@utulsa.edu>
2024-03-07 22:23:03 -0600

    prob 7

--- Problem#7/carwash.py
+++ Problem#7/carwash.py
@@ -2,18 +2,46 @@
 def calculate_car_wash_price(service_choice1, service_choice2):
     services = {'Air freshener': 1, 'Rain repellent': 2, 'Tire shine': 2, 'Wax': 3, 'Vacuum': 5}
     base_wash = 10
-    total = 0
-    output = "ZyCar Wash\n"
+    total = 10
+    output = "ZyCar Wash \nBase car wash - $10"
    #type your code here 
     if service_choice1 == "Air freshener":
-        output += "Air freshener - 1"
+        output += "\nAir freshener - $1"
         total += 1
-    elif service_choice1 == "Air freshener":
-        output += "Air freshener - 1"
+    elif service_choice1 == "Rain repellent":
+        output += "\nRain repellent - $2"
+        total += 2
+    elif service_choice1 == "Tire shine":
+        output += "\nTire shine - $3"
+        total += 3
+    elif service_choice1 == "Wax":
+        output += "\nWax - $3"
+        total += 3
+    elif service_choice1 == "Vaccum":
+        output += "\nVaccum - $5"
+        total += 3
+    else:
+        output += "\n-----"
+    if service_choice2 == "Air freshener":
+        output += "\nAir freshener - $1"
         total += 1
+    elif service_choice2 == "Rain repellent":
+        output += "\nRain repellent - $2"
+        total += 2
+    elif service_choice2 == "Tire shine":
+        output += "\nTire shine - $3"
+        total += 3
+    elif service_choice2 == "Wax":
+        output += "\nWax - $3"
+        total += 3
+    elif service_choice2 == "Vaccum":
+        output += "\nVaccum - $5"
+        total += 3
+    else:
+        output += "\n-----"
     
-    print(services['Air freshener'])
-    output += ("-----\nTotal price:" + total)
+    
+    output += ("\n-----\nTotal price: $" + str(total))
     print(output)
 if __name__ == '__main__':
     # Get user input for service choices
